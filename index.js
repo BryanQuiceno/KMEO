b9ccd844d42f875f510207efcea70f12a3282c41
bryan <bryanquiceno0@gmail.com>
2024-01-18 23:20:33 +0100

    creacion del menu desplegable, el section contacto el footer y ajuste de la relacion de apsecto del iframe de youtube

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const playSong = document.getElementsByClassName("play");
 const stopSong = document.getElementsByClassName("stop");
 const friky = document.getElementsByClassName("song");
 const volume = document.querySelector(".volume");
+const menuBtn = document.getElementsByClassName("menu-btn");
+const buttonMenu = document.getElementById("buttonMenu");
+const barraNavegacion = document.getElementById("barra-navegacion");
+let transform = "rotate(0deg)";
 
 let nombre = "Friky";
 let cancion = new Audio(`audio/${nombre}.mp3`);
@@ -38,3 +42,26 @@ for (let elemento of friky) {
 volume.addEventListener("click", function () {
   cancion.volume = this.value;
 });
+console.log(window.getComputedStyle(buttonMenu));
+console.log(window.getComputedStyle(buttonMenu).zIndex);
+for (let elemento of menuBtn) {
+  elemento.addEventListener("click", function () {
+    barraNavegacion.style.transition = ".5s";
+    // barraNavegacion.classList.toggle("active");
+    buttonMenu.style.transition = ".5s";
+
+    if (transform === "rotate(0deg)") {
+      barraNavegacion.style.display = "flex";
+      barraNavegacion.style.transition = ".5s";
+      barraNavegacion.style.transform = "translateX(100%)";
+
+      buttonMenu.style.transform = "rotate(90deg)";
+      transform = "rotate(90deg)";
+    } else {
+      barraNavegacion.style.transform = "translateX(-100%)";
+      barraNavegacion.style.transition = ".5s";
+      buttonMenu.style.transform = "rotate(0deg)";
+      transform = "rotate(0deg)";
+    }
+  });
+}
